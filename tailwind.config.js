d44751de692ee8b732a14011c502dbd9cd217c42
Ebube111 <ebubeagwaze@gmail.com>
2021-03-05 10:08:28 +0100

    Sanity Fresh

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    prefix: 'tw-',
+  theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+    }
+  }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
